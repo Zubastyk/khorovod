0fe49132620392f0f07d43e36765b3033f63f5fa
Zubastyk <zubbastyk@gmail.com>
2024-01-26 01:29:30 +0300

    работает по старту определяет пользователей сохраняет в строку в excel сделать нумерацию и в столбец

--- main.py
+++ main.py
@@ -9,8 +9,30 @@ from bot_profile import *
 bot = TelegramClient('khorovod', api_id, api_hash).start(bot_token=bot_token)
 
 
+@bot.on(events.NewMessage(pattern='/start'))
 async def main(event):
+    await event.respond("Привет")
     chat = await event.get_chat()
+    await event.respond("Узнаем пользователей...")
+    all_participants = []
+    all_participants = await bot.get_participants(chat)
+    
+    # Создаем экземпляр пустой книги Excel
+    wb = openpyxl.Workbook()
+    ws = wb.active
+    ws.title = "Список участников"
+    #ws.append(("username"))
+    user_list = []
+    for user in all_participants:
+            if user.username:
+                username = user.username
+            else:
+                username = ""
+            user_list.append(username)
+    ws.append(user_list)
+    await event.respond("Сохраняем лист...")
+    wb.save('member.xlsx')
+    await event.respond("Сохраняем файл...")
 
 
 
